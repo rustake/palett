f3006b63de581d592ae7d7cea6252246c246c99c
Hoyeung <hoyeungw@outlook.com>
2021-02-14 14:16:44 +0800

    minor fix

--- src/convert/_rgb_hex.rs
+++ src/convert/_rgb_hex.rs
@@ -15,14 +15,14 @@ mod tests {
     fn test_convert() {
         // let mut book_reviews = HashMap::new();
         let mut map = HashMap::new();
-        map.insert("black", (0u8, 0u8, 0u8));
+        map.insert("BLACK", (0u8, 0u8, 0u8));
         map.insert("RED", (255u8, 0u8, 0u8));
         map.insert("GREEN", (0u8, 255u8, 0u8));
         map.insert("BLUE", (0u8, 0u8, 255u8));
         map.insert("YELLOW", (255u8, 255u8, 0u8));
-        map.insert("magenta", (255u8, 0u8, 255u8));
+        map.insert("MAGENTA", (255u8, 0u8, 255u8));
         map.insert("CYAN", (0u8, 255u8, 255u8));
-        map.insert("white", (255u8, 255u8, 255u8));
+        map.insert("WHITE", (255u8, 255u8, 255u8));
         // println!("{:?}", map);
         for (key, value) in map {
             println!("{} : {:?}, {:?}", key, value, rgb_hex(&value));
